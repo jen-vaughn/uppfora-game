f796abee5a2e1b5f877c0cbd60197eb5e6ebd952
jen-vaughn <93220451+jen-vaughn@users.noreply.github.com>
2022-11-13 12:02:01 -0500

    Update Uppfora.java

--- Uppfora.java
+++ Uppfora.java
@@ -3,26 +3,11 @@ import java.util.Random;
 
 public class Uppfora {
 	// Jennifer Vaughn
-	// CIS 1068, Section 1
 	// 9/26/22
-	// Assignment 4: a game
-	/* This program will allow the user to play the game
+	// Uppföra Game
+	/* This program allows the user to play the game
 		of Uppföra against the computer. */
 	
-	
-	/* Outline:
-		Print rules.
-		Ask user if they want to play.
-		Checks if user's response is valid and ask to correct it if not.
-		While user wants to play:
-		- Ask user to enter their move
-		- Check that user's chosen move is valid and ask to correct it if not
-		- Generate a random move for the computer
-		- Use conditionals to decide who wins and loses. If computer wins, it's a tie.
-		- For every round, print user move, computer move, and the winner
-		- At end of each round, ask if user still wants to play
-		...If user inputs 'y', while loop should repeat.
-		...If user inputs 'n', print the number of rounds, user wins, and computer wins. */
 	
 	
 	public static Scanner CONSOLE = new Scanner(System.in);
@@ -54,14 +39,14 @@ public class Uppfora {
 	
 	
 	public static void playRound()	{
-		//Ask user if they want to play
+		//Asks user if they want to play
 		System.out.println("Would you like to play a round? Type 'y' or 'n': ");
 		String play = CONSOLE.nextLine();
 		play = validResponse(play);			//Checks if user's 'y' or 'n' response is valid
 		
 		
 			
-		//Setting up int variables
+		//Sets up int variables
 		int numRounds = 0;
 		int userWins = 0;
 		int compWins = 0;
@@ -73,7 +58,6 @@ public class Uppfora {
 		while(play.equals("y"))	{
 			numRounds += 1;
 			
-			//Declaring variables
 			String compChoice = "";
 			String winner = "";
 			
@@ -85,11 +69,11 @@ public class Uppfora {
 			//Random computer move
 			compChoice = compRandChoice();
 				
-			//Echoing user and computer moves
+			//Echos user and computer moves
 			System.out.println("Your move is " + userChoice);
 			System.out.println("The computer's move is " + compChoice);
 				
-			//Execute the game with rules
+			//Executes the game
 			winner = Game(userChoice, compChoice);
 			
 			if(winner.equals("user"))	{
@@ -99,10 +83,10 @@ public class Uppfora {
 					compWins += 1;
 				}
 			
-			//State the winner of the game
+			//States the winner of the game
 			System.out.println("The winner is the " + winner);
 				
-			//Ask user if they want to play again	
+			//Asks user if they want to play again	
 			System.out.println();
 			System.out.println("Would you like to play again? Type 'y' or 'n': ");
 			play = CONSOLE.nextLine();
